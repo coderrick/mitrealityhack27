8bdd8ae7b0240f859d7dc17190dc879c85ccd61b
coderrick <derrick.duncan@temple.edu>
2020-01-18 17:06:49 -0500

    Nreal project updates

---
+++ unity/nreal/Assets/switching.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switching : MonoBehaviour
+{
+    public string level_name = "Input-ControllerInfo";
+    public AudioSource a1;
+    // public GameObject SphereToChange;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   public void ChangeScene(){
+        // SphereToChange.GetComponent<Renderer>().material.color = Color.red;
+        Application.LoadLevel(level_name);
+        
+   }
+
+   public void PlayBackgroundMusic(){
+      a1.Play();
+   }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
