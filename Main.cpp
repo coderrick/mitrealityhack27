00a62f3a4577141a52e2b20e409d7968613c65ae
Tim Range <42386765+timrange@users.noreply.github.com>
2020-01-19 12:32:44 -0500

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "App.h"
+
+struct RoInitializeWrapper
+{
+    inline RoInitializeWrapper() { RoInitialize(RO_INIT_MULTITHREADED); }
+    inline ~RoInitializeWrapper() { RoUninitialize(); }
+};
+
+int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+    RoInitializeWrapper roInit;
+
+    Windows::ApplicationModel::Core::CoreApplication::Run(ref new Mixed_Reality_Sign_Language_Learning_Studio::App());
+    return 0;
+}
